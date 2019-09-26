c26031004a1512664e2ffa4fab639a36312ddfd3
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-09-26 13:54:32 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+#def prime?(num)
+#p = 2
+#prime = (-1..num).to_a
+#prime.each do
+#end
+def prime?(num)
+  arr = (2..num-1)
+  count = 0
+  while arr.each do |p|
+  if p <= 1
+    count += 1
+    return false
+  else
+    return true
+  end
+end
